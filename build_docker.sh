9e5f4bf2203b24c114394848f38e84cfbb63a4f0
runshine <woshiyuhao0819@gmail.com>
2023-10-28 00:10:39 +0800

    add multi arch docker image support

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build . -f smartdns.dockerfile -t runshine0819/smartdns:$(uname -m)
-docker build . -f smartdns-server.dockerfile -t runshine0819/smartdns-server:latest --network host
+docker buildx build . -f smartdns.dockerfile        -t runshine0819/smartdns:latest        --platform linux/amd64,linux/arm64,linux/arm --push
+docker buildx build . -f smartdns-server.dockerfile -t runshine0819/smartdns-server:latest --platform linux/amd64,linux/arm64,linux/arm --push
